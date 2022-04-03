17f440951e51ca4941b9fa3ddcff2c17d22e5700
stefluka <lukaszstefanek94@gmail.com>
2022-04-03 15:09:45 +0200

    Initial commit

---
+++ src/main/java/com/stefanek/lukasz/restcqrs/RestCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.stefanek.lukasz.restcqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCqrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestCqrsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stefanek/lukasz/restcqrs/RestCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefanek.lukasz.restcqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCqrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
